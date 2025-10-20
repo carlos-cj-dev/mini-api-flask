ac513130ac5977ffb959ddc67fb734498f061931
Arthur-Developerr <arthurfurtado@cest.edu.br>
2025-10-20 15:59:49 -0300

    feat: add user schema

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -7,4 +9,11 @@ class Base(DeclarativeBase):
 
 
 db = SQLAlchemy(model_class=Base)
+
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]= mapped_column(String)
+    email: Mapped[str] = mapped_column(unique=True)
 
